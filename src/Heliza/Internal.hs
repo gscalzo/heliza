8a03ffb2ceb4daee5df460c02dae7facb565a1c8
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-14 15:51:02 +0000

    adds upCase

--- src/Heliza/Internal.hs
+++ src/Heliza/Internal.hs
@@ -1,15 +1,20 @@
 module Heliza.Internal
     (
         compatter,
+        upCase,
         pippo,
         firstWord
     ) where
 
+import Data.Char
 
 compatter :: String -> String
 compatter []        = []
 compatter ('\'':xs) = compatter xs
 compatter (x:xs)    = x : compatter xs    
+
+upCase :: String -> String
+upCase = map toUpper
 
 
 pippo :: Integer -> [Char]
--- test/HelizaSpec.hs
+++ test/HelizaSpec.hs
@@ -13,6 +13,10 @@ spec = do
     it "should return string without apostrophes" $ do
         (compatter "I'M A IB'M 37'0") `shouldBe` "IM A IBM 370"
 
+  describe "upCase" $ do
+    it "should return string in uppercase" $ do
+        (upCase "im a ibm 370") `shouldBe` "IM A IBM 370"
+
   describe "firstWord" $ do
     it "should return first word of a list of words" $ do
         file <- getDataFileName "data/keywords.txt"
