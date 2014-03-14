0b7ee50de89868474983d7aa5877640f884f5e8b
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-14 15:58:25 +0000

    adds two safe tests

--- test/HelizaSpec.hs
+++ test/HelizaSpec.hs
@@ -12,10 +12,14 @@ spec = do
   describe "compact" $ do
     it "should return string without apostrophes" $ do
         (compact "I'M A IB'M 37'0") `shouldBe` "IM A IBM 370"
+    it "should handle empty string" $ do
+        (compact "") `shouldBe` ""
 
   describe "upCase" $ do
     it "should return string in uppercase" $ do
         (upCase "im a ibm 370") `shouldBe` "IM A IBM 370"
+    it "should handle empty string" $ do
+        (upCase "") `shouldBe` ""
 
   describe "firstWord" $ do
     it "should return first word of a list of words" $ do
